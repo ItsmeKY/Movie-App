d315df63624d64fb1f297c4c184df95e95606247
ItsmeKY3 <yousifkurdy3@gmail.com>
2023-09-23 02:56:23 +0300

    Initial Commit

---
+++ Movie App/Movie_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_AppApp.swift
+//  Movie App
+//
+//  Created by ItsmeKY3 on 9/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Movie_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
